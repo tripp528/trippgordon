20403daf27085e72422ab9911324c46438047f91
tripp528 <tripp528@gmail.com>
2018-10-22 00:56:28 -0400

    new production page

--- trippgordon/settings.py
+++ trippgordon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'music',
     'personal',
     'django.contrib.admin',
     'django.contrib.auth',
--- trippgordon/urls.py
+++ trippgordon/urls.py
@@ -20,4 +20,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('personal.urls')),
+    path('music/', include('music.urls')),
+
 ]
